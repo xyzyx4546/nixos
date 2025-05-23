b856adeddea58fae82b8f1eec89f1850334c177e
David Ehrhardt <d.ehrhardt@proton.me>
2025-05-23 12:33:51 +0200

    deps: update flake inputs

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -130,7 +130,7 @@
         };
       }];
 
-      initExtra = ''
+      initContent = ''
         # history substring search
         bindkey "$terminfo[kcuu1]" history-substring-search-up
         bindkey "$terminfo[kcud1]" history-substring-search-down
