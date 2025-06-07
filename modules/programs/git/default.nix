c40eaebef88da86c1492b8e2155df7e2a2ec6cab
David Ehrhardt <d.ehrhardt@proton.me>
2025-06-07 19:54:20 +0200

    feat: switch from gitlab to github

--- modules/programs/git/default.nix
+++ modules/programs/git/default.nix
@@ -1,8 +1,4 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    glab
-  ];
-
+{pkgs, ...}: {
   programs = {
     git = {
       enable = true;
@@ -10,29 +6,61 @@
       userEmail = "d.ehrhardt@proton.me";
       extraConfig = {
         credential.helper = "store";
-        credential."https://gitlab.com".helper = "${pkgs.glab}/bin/glab auth git-credential";
         pull.rebase = true;
         init.defaultBranch = "main";
       };
     };
 
-    lazygit = {
+    gh = {
       enable = true;
-
+      extensions = with pkgs; [
+        gh-eco
+        gh-f
+        gh-i
+        gh-s
+      ];
+    };
+    gh-dash = {
+      enable = true;
       settings = {
         theme = {
-          activeBorderColor = [ "#FF79C6" "bold" ];
-          inactiveBorderColor = [ "#BD93F9" ];
-          searchingActiveBorderColor = [ "#8BE9FD" "bold" ];
-          optionsTextColor = [ "#6272A4" ];
-          selectedLineBgColor = [ "#6272A4" ];
-          inactiveViewSelectedLineBgColor = [ "bold" ];
-          cherryPickedCommitFgColor = [ "#6272A4" ];
-          cherryPickedCommitBgColor = [ "#8BE9FD" ];
-          markedBaseCommitFgColor = [ "#8BE9FD" ];
-          markedBaseCommitBgColor = [ "#F1FA8C" ];
-          unstagedChangesColor = [ "#FF5555" ];
-          defaultFgColor = [ "#F8F8F2" ];
+          ui.table.compact = true;
+          colors = {
+            text = {
+              primary = "#f8f8f2";
+              secondary = "#bd93f9";
+              inverted = "#282a36";
+              faint = "#f1fa8c";
+              warning = "#ff5555";
+              success = "#50fa7b";
+            };
+            background.selected = "#44475a";
+            border = {
+              primary = "#44475a";
+              secondary = "#44475a";
+              faint = "#44475a";
+            };
+          };
+        };
+      };
+    };
+
+    lazygit = {
+      enable = true;
+      settings = {
+        theme = {
+          activeBorderColor = ["#FF79C6" "bold"];
+          inactiveBorderColor = ["#BD93F9"];
+          searchingActiveBorderColor = ["#8BE9FD" "bold"];
+          optionsTextColor = ["#6272A4"];
+          selectedLineBgColor = ["#6272A4"];
+          inactiveViewSelectedLineBgColor = ["bold"];
+          cherryPickedCommitFgColor = ["#6272A4"];
+          cherryPickedCommitBgColor = ["#8BE9FD"];
+          markedBaseCommitFgColor = ["#8BE9FD"];
+          markedBaseCommitBgColor = ["#F1FA8C"];
+          unstagedChangesColor = ["#FF5555"];
+          defaultFgColor = ["#F8F8F2"];
         };
         keybinding.universal = {
           nextBlock-alt2 = "]";
