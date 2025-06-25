fd181863616fca2a15a6f32a3f9afc159e4a716f
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2025-06-25 14:33:16 +0200

    chore: remove gamescope

--- hosts/common-graphical.nix
+++ hosts/common-graphical.nix
@@ -74,7 +74,6 @@
         };
       };
     };
-    gamescope.enable = true;
   };
 
   services = {
--- modules/system/hyprland/common.nix
+++ modules/system/hyprland/common.nix
@@ -101,9 +101,9 @@
         "dimaround, class:(floating|xdg-desktop-portal-gtk|zenity|polkit-gnome-authentication-agent-1)"
         "size 800 450, class:(floating|xdg-desktop-portal-gtk|zenity|polkit-gnome-authentication-agent-1)"
 
-        "workspace special:games, class:^(surviving mars$|Minecraft.*|ksp.x86_64.*|steam_app.*|gamescope$)"
-        "tile, class:^(surviving mars$|Minecraft.*|ksp.x86_64.*|steam_app.*|gamescope$)"
-        "fullscreen, class:^(surviving mars$|Minecraft.*|ksp.x86_64.*|steam_app.*|gamescope$)"
+        "workspace special:games, class:^(surviving mars|Minecraft.|ksp.x86_64.|steam_app.)*"
+        "tile, class:^(surviving mars|Minecraft.|ksp.x86_64.|steam_app.)*"
+        "fullscreen, class:^(surviving mars|Minecraft.|ksp.x86_64.|steam_app.)*"
       ];
 
       workspace = [
@@ -191,9 +191,6 @@
         disable_hyprland_logo = true;
         force_default_wallpaper = 0;
       };
-
-      # HACK: fix for gamescope
-      debug.full_cm_proto = true;
 
       debug.disable_logs = false;
     };
