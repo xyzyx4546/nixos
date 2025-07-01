fbc384ac18753047994e89d4059d8633bc5cc6f6
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2025-07-01 15:37:43 +0200

    fix: replace toggleterm.lazygit with custom impl

---
+++ modules/programs/neovim/lua/terminals.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable: undefined-global
+-- luacheck: globals vim
+
+local Terminal = require("toggleterm.terminal").Terminal
+
+local terminals = {
+	{ cmd = "lazygit", key = "l" },
+	{ cmd = "gh-dash", key = "g" },
+	{ cmd = "btop", key = "d" },
+}
+
+for _, term in ipairs(terminals) do
+	vim.keymap.set("n", "<leader>t" .. term.key, function()
+		Terminal:new({ cmd = term.cmd, hidden = true }):toggle()
+	end, { desc = "Open " .. term.cmd .. " [toggleterm]" })
+end
