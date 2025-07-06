9f02130016b3fec1ed9c8de719ed5b3fe8490ed9
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2025-07-06 14:24:45 +0200

    feat: add kitty cursor trail

--- modules/programs/kitty/default.nix
+++ modules/programs/kitty/default.nix
@@ -11,6 +11,8 @@
       remember_window_size = "no";
       background_opacity = 0;
       touch_scroll_multiplier = 20;
+      cursor_trail = 1;
+      cursor_trail_start_threshold = 5;
 
       confirm_os_window_close = 0;
       enable_audio_bell = "no";
