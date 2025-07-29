e23c545e59d430a93c4a23a55a3831749fccc53d
David Ehrhardt <d.ehrhardt@proton.me>
2025-07-29 13:53:28 +0200

    fix: remove .zip file workaround

--- modules/programs/yazi/default.nix
+++ modules/programs/yazi/default.nix
@@ -40,20 +40,8 @@
       '';
 
     settings = {
-      # HACK: file is broken for .zip files in version 5.46
-      # https://bugs.astron.com/view.php?id=571
-      open.prepend_rules = [
-        {
-          name = "*.zip";
-          use = "extract";
-        }
-      ];
       plugin = {
         prepend_previewers = [
-          {
-            name = "*.zip";
-            run = "ouch";
-          }
           {
             mime = "application/*zip";
             run = "ouch";
@@ -75,7 +63,27 @@
             run = "ouch";
           }
           {
+            mime = "application/vnd.rar";
+            run = "ouch";
+          }
+          {
             mime = "application/x-xz";
+            run = "ouch";
+          }
+          {
+            mime = "application/xz";
+            run = "ouch";
+          }
+          {
+            mime = "application/x-zstd";
+            run = "ouch";
+          }
+          {
+            mime = "application/zstd";
+            run = "ouch";
+          }
+          {
+            mime = "application/java-archive";
             run = "ouch";
           }
         ];
