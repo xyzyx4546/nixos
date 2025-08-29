3daa2acf50d039b3a912abb13fed20bd11e790f6
David Ehrhardt <d.ehrhardt@proton.me>
2025-08-29 19:46:41 +0200

    deps: update flake inputs

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -24,7 +24,7 @@
   home.file.".mozilla/firefox/xyzyx/chrome" = {
     source = "${builtins.fetchGit {
       url = "https://github.com/amnweb/firefox-plus.git";
-      rev = "56709c4b0fd056bde8fee531dfaab1a58385c2cb";
+      rev = "9954a13541a1d8ca7e84bbb58fce68d60c517490";
     }}/chrome";
   };
 }
