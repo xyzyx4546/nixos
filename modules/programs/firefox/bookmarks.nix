47efb561988d96e834ce5f5fbeb81ea2c4979faa
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2025-11-12 17:36:03 +0100

    feat: adjust firefox bookmarks

--- modules/programs/firefox/bookmarks.nix
+++ modules/programs/firefox/bookmarks.nix
@@ -20,9 +20,10 @@ in {
           (folder "Studium" [
             (item "Moodle" "https://elearning.dhbw-stuttgart.de/moodle/my/courses.php")
             (item "Dualis" "https://dualis.dhbw.de/")
+            (item "Rapla" "https://rapla.dhbw.de/rapla/calendar?key=5rBgzE8vgPbhgXiJyxx-0UCbHyoc66RNQpI_KSJSTzwKhmhw9TmBpAxkTFcX3ekGZvmMIfKjixjI4sqYWjh8QSKk6Roj-wReI77MwBFOLM16mUJlJTz7JJ4o62NFiY5fGryjvwt1kpad5g93Dkdn0A&salt=-231759520")
+            (item "E-Mail" "https://lehre-webmail.dhbw-stuttgart.de/roundcubemail")
           ])
           (item "GitHub" "https://github.com")
-          (item "YouTube" "https://www.youtube.com/feed/subscriptions")
           (item "Nextcloud" "https://fam-ehrhardt.de")
         ];
       }
