5c571349714e0079b5cc159fb1e35774bc7dc5ab
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2026-06-08 13:53:06 +0200

    feat: add user to podman group

--- hosts/common.nix
+++ hosts/common.nix
@@ -54,7 +54,7 @@
 
   users.users.xyzyx = {
     isNormalUser = true;
-    extraGroups = ["wheel" "input" "networkmanager"];
+    extraGroups = ["wheel" "input" "networkmanager" "podman"];
     hashedPasswordFile = config.sops.secrets."xyzyx/password".path;
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOq2xd3Eri9HfFP49Gl4snnrxMY6zXyNpWQIs9dd2L4Q"];
