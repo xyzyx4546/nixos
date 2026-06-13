81389f694990a4907adfc4273ea74e02856649e1
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2026-06-13 16:31:23 +0200

    feat: enable nix-ld

--- hosts/common.nix
+++ hosts/common.nix
@@ -62,6 +62,10 @@
 
   programs = {
     zsh.enable = true;
+    nix-ld = {
+      enable = true;
+      libraries = [(pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")];
+    };
     nh = {
       enable = true;
       flake = "/home/xyzyx/Projects/nixos";
