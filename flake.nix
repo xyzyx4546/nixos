b24720ef85d3e0a9d3030206b647d419acfd30ec
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2026-07-19 19:45:20 +0200

    deps: update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixos-raspberrypi = {
       url = "github:nvmd/nixos-raspberrypi";
       inputs.nixpkgs.follows = "nixpkgs";
