278a5b51fb528bc51b04ffed0f6c8c247c83e9dc
David Ehrhardt <93977476+xyzyx4546@users.noreply.github.com>
2026-08-20 21:58:06 +0200

    fix: use OSC 52 for nvim clipboard

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -31,9 +31,24 @@
 
       clipboard = {
         enable = true;
-        providers.wl-copy.enable = true;
         registers = "unnamedplus";
       };
+      luaConfigRC.osc52-clipboard =
+        # lua
+        ''
+          vim.g.clipboard = {
+            name = 'OSC 52',
+            copy = {
+              ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+              ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+            },
+            paste = {
+              ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+              ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+            },
+          }
+        '';
+
       lineNumberMode = "relNumber";
 
       globals = {
